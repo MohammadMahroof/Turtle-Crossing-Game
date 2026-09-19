9a762f4d867876fcd9a7a358cf54b46cce493da0
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-19 15:55:11 +0530

    chore: ignore Python cache file

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-player = Player()
-car_manager = CarManager()
-
-screen.listen()
-screen.onkey(player.go_up, "Up")
-
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
